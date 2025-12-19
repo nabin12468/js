2ee28801079fd4fec894f90626aeff3f20fbcd0b
nabin12468 <nabindamase12345@gmail.com>
2025-12-19 18:01:41 +0545

    input

--- first.js
+++ first.js
@@ -1,1 +1,12 @@
-//Function-> 2 numbers,sum
+// design pattern 
+// module pattern
+ // module pattern ek design pattern hai jisme hum apne code ek self executing function ke andar likhte hain,taki variable aur functions private rahen.
+
+ // iske andar se hum sirf wahi cheeze bahar return karte hain jo humein chahiye hoti hain.
+
+ //Is psttern ka main fayda hai data hiding (encapsulation) aur clean structure,taaki code secure,reusable aur manageable ban sake.
+
+ let c = "nabin"
+ let b= "shrestha"
+
+ 
