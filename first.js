e80fe547dc5de5d4260094715886a4bc15bb5d3f
nabin12468 <nabindamase12345@gmail.com>
2025-12-28 14:36:36 +0545

    Temporal Dead Zone

--- first.js
+++ first.js
@@ -6,6 +6,21 @@
 
  //Is psttern ka main fayda hai data hiding (encapsulation) aur clean structure,taaki code secure,reusable aur manageable ban sake.
 
- function (){
-    let a =12
- }
+  
+ 
+ /*var a = 12; // var use for declare and value put for initialize
+ a = 13; // reassign value
+
+ var b = 14;  // redeclar
+ console.log(b);     
+ 
+
+ this is a problem so that fix by let
+ 
+ */
+
+ //Temporal Dead Zone (TDZ)
+   //let and const ke sath hum TDZ ka samna karte hain,jo ek aisa period hota hai jisme variable declare to ho chuka hota hai lekin uska initialization nahi hua hota.
+
+   console.log(x); // ReferenceError: Cannot access 'x' before initialization
+   let x = 10;
