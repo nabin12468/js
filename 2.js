8429ffc89341ce24397458d6096d530badfa8359
nabin12468 <nabindamase12345@gmail.com>
2025-12-29 15:57:41 +0545

    primitive data type

--- 2.js
+++ 2.js
@@ -1,6 +1,28 @@
 //2.Data Type + Type System
-//Primitive Data Types
+
+
+//Primitive Data Types  -> aisi saari values jinko copy karne par tumhe ek real copy mil jaaye
+// number, string, boolean, null, undefined, symbol, bigInt
+
+
+// reference  -> inko copy karne par real copy nahi milegi but aapko reference milega parent ka
+
+// array, object, functions
+// [] {} ()
+
+
+/*  let a = 10; // number
+let b=a; // copy by value
+ a= a + 5;
+    console.log(a); // 15  */
 
 
 
+/*let a = [1,2,3]; // reference type
+let b = a; // copy by reference
+a.push(4);
+console.log(a); // [1,2,3,4]
+*/
 
+// number, string, boolean, null, undefined, symbol, bigInt
+// array, object, functions
