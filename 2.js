073299246b8560b781a1d5de0c2bc15fbf43dcbb
nabin12468 <nabindamase12345@gmail.com>
2025-12-31 15:01:34 +0545

    dynamic type

--- 2.js
+++ 2.js
@@ -52,15 +52,30 @@ console.log(a); // [1,2,3,4]
 
 
   // bigint
-  let  a= 987604836447381n;
+ /* let  a= 987604836447381n;
   a =2 +a;
-  console.log(a);
-  
+  console.log(a); */
+   
+
 
 
 
 
 
 // array, object, functions
+
+
+
+
+//Dynamic typing  -> js mein static typing nahi hai and yaha par hai
+//dynamic typing jiska matlab hai aap data ko change kr skte ho kunki yahha par dynamic data types hai
+
+let a = 10; // number
+a =true; // boolean
+a ="Naveen"; // string
+a = null; // null
+a = undefined; // undefined
+
+
 
 
