b909a15235377a0548062a173b7b8b2f6d757b1b
nabin12468 <nabindamase12345@gmail.com>
2026-01-01 15:25:33 +0545

    arithmetic

---
+++ operator.js
@@ -0,0 +1,8 @@
+// Operators in JavaScript
+
+    // arithmetic operators,comparison operators,logical operators,bitwise operators,assignment operators,string operators,conditional (ternary) operators,type operators,comma operators,unary operators
+
+
+    // arithmetic operators,
+
+    
