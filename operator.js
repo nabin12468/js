5b424fdeb161a213bd25f3b823d468d6e689c3f4
nabin12468 <nabindamase12345@gmail.com>
2026-01-02 12:46:56 +0545

    typeof

--- operator.js
+++ operator.js
@@ -5,4 +5,31 @@
 
     // arithmetic operators,
 
-    
+    /*12 = 13 //false
+    12 = "12" //true
+    12 === "12" //false
+
+
+    */
+
+    //unary operators
+
+   /* typeof "hello" //string
+    typeof 12 //number
+    typeof true //boolean
+    typeof {} //object
+    typeof [] //object
+    typeof undefined //undefined
+    typeof null //object
+
+    let a=13;
+    ++a; //13
+    a++; //14  */
+
+
+    //tetnary operators
+
+
+12>13 ? console.log("true") : console.log("false");  
+   
+
