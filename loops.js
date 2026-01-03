0bd23b71565ae2e8ea52a0e040b25706ab9b747e
nabin12468 <nabindamase12345@gmail.com>
2026-01-03 14:24:30 +0545

    for

---
+++ loops.js
@@ -0,0 +1,31 @@
+// loops
+
+//repeat karne ko loop kahte hai
+//for while do-while forin forof foreach
+
+//1111111  1 2 3 4 5 // kaha se jaana hai  -> kaha tak jaana hai -> kaise jaana hai
+//for 
+// 1-50
+
+ // kaha se jaana hai  -> kaha tak rukna hai -> kaise jaana hai
+//while
+
+// bro mat bol bhai nai tera -while
+
+
+//for
+
+//for(let start,end,change)
+//1-100
+
+ /*  
+ for(let i=1;i<101; i++){
+
+    console.log(i)
+
+}
+      */
+
+
+
+
