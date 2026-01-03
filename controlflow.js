bf78d0fe769be671c5376867d11d56be20379e04
nabin12468 <nabindamase12345@gmail.com>
2026-01-03 13:50:33 +0545

    rock-paper-scissor

--- controlflow.js
+++ controlflow.js
@@ -3,6 +3,8 @@
 // early return psttern
 
 
+/*
+
 switch (1) {
     case 1:
         console.log("one");
@@ -18,3 +20,51 @@ switch (1) {
 
 
 
+// early return pattern
+
+function example(age) {
+    if (age<25)  return "A";
+    else if (age<50)  return "B";
+    else if (age<70)  return "C";
+    else return "D"
+}
+console.log(example(12))  
+
+
+// (Q)
+function getGrade(score){
+    if (score>=90 && score <=100) 
+        
+            return "A+";
+     if (score>=80 && score <=89)return "A";
+     if (score>=60 && score <=79)return "B+";
+     if (score>=50 && score <=69) return "B";
+     if (score>=40 && score <=59) return "fail";
+    
+    
+    else return "Invalid marks"
+
+    
+
+    
+
+    
+
+} 
+
+console.log(getGrade(11))
+
+*/
+
+//rock paper scissor
+
+function rps (user,computer){
+
+    if (user === computer) return "draw"
+    if(user ==="rock" && computer === "scissor") return "user";
+    if(user ==="paper" && computer === "rock") return "user";
+    if(user ==="scissor" && computer === "paper") return "user";
+    return "computer"
+}
+
+console.log (rps("rock","paper"))
