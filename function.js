eafd76a0bbd842380defbbaab308c5490b022e93
nabin12468 <nabindamase12345@gmail.com>
2026-01-05 21:45:26 +0545

    function

---
+++ function.js
@@ -0,0 +1,26 @@
+// what why how
+
+// we have code jun code lekhda bitikai chalauna hoina after call paxi chalauna ka lagi function banako ho
+
+/**
+ 
+function abcd(){
+//function statment
+}
+
+ 
+
+let fnc = function(){
+    console.log("dance")
+ // function expression
+}
+fnc() */
+
+// fat arrow fnc
+
+let fnc = () =>{
+
+    console.log("you are beautifull")
+
+};
+fnc();
