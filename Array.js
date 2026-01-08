3ef3d551c0010bfb1bb5c437c3c1aea64d0704e1
nabin12468 <nabindamase12345@gmail.com>
2026-01-08 14:45:41 +0545

    push,pop,shift

---
+++ Array.js
@@ -0,0 +1,21 @@
+//array
+
+/*
+
+let marks = [1,2,3,4];
+arr[2]=5;//add the array
+console.log(marks);
+
+
+
+let arr = [1,2,3,4,5]
+arr.push(299)
+console.log(arr)
+arr.pop(2)
+console.log(arr)
+
+*/
+
+let arr = [1,2,3,4,5]
+arr.shift();
+console.log(arr)
