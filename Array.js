89848744cb00d3b348034985b5e59f9f12d3044f
nabin12468 <nabindamase12345@gmail.com>
2026-01-08 15:05:22 +0545

    sort

--- Array.js
+++ Array.js
@@ -14,8 +14,49 @@ console.log(arr)
 arr.pop(2)
 console.log(arr)
 
-*/
+
 
 let arr = [1,2,3,4,5]
 arr.shift();
 console.log(arr)
+
+
+
+let arr = [1,2,3,4,5]
+arr.unshift(0);
+console.log(arr)
+
+
+
+let arr = [1,2,3,4,5]
+arr.splice(2,2);
+console.log(arr)
+
+
+
+
+let arr = [1,2,3,4,5];
+let newarr = arr.slice(0,3);
+console.log(newarr)
+
+
+
+
+
+let arr = [1,2,3,4,5];
+arr.reverse();
+console.log(arr);
+
+
+
+let arr = [1,6,3,4,5];
+arr.sort(function(a,b){
+    return b-a
+});
+console.log(arr);
+
+*/
+
+
+
+
