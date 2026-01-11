6055574523290c06b68aa167699373e89c83af27
nabin12468 <nabindamase12345@gmail.com>
2026-01-11 19:27:22 +0545

    getAttribute,removeAttribute

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ let h1 = document.querySelector("h1");
 //h1.innerHTML = "<i>hey</>";
 
 
-*/
+
 
 //Attribute manipulation:getAttribute,setAttribute,removeAttribute
 
@@ -47,4 +47,20 @@ let a = document.querySelector("a");
 //console.log(a);
 //a.href = "https://www.google.com"
 a.setAttribute("href","https://www.google.com");
+
+
+
+// setAttribute
+let img = document.querySelector("img");
+img.setAttribute("src", "https://plus.unsplash.com/premium_photo-1670275658563-450157cc6e34?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
+
+//getAttribute
+let a = document.querySelector("a")
+console.log(a.getAttribute("href"));
+
+*/
+//removeAttribute
+let a = document.querySelector("a")
+a.removeAttribute("href");
 
