0daaacf8df34dd909b275d5a1bceb25b140a5976
nabin12468 <nabindamase12345@gmail.com>
2026-01-15 14:21:18 +0545

    submit

--- script1.js
+++ script1.js
@@ -124,7 +124,14 @@ fileinp.addEventListener("change", function(dets){
 
 
 let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", function(dets){
     dets.preventDefault();
+    console.log(
+        inputs[0].value,
+        inputs[1].value,
+        inputs[2].value,
+        inputs[3].value,
+    );
 })
