2b6b7a14dc91e1f1e69a7620afe6c6a33dff0ae6
nabin12468 <nabindamase12345@gmail.com>
2026-01-18 20:45:56 +0545

    constbregex

--- script2.js
+++ script2.js
@@ -22,3 +22,13 @@ form.addEventListener("submit", function (dets){
 
 //js-based vallidation
 
+let nm = document.querySelector("#name");
+let form= document.querySelector("form");
+
+form.addEventListener("submit", function(dets){
+    dets.preventDefault();
+
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let ans=  regex.test("harsh@test.com")
+    console.log(ans);
+});
