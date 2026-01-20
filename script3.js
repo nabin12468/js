381c1a6c3802b92c43d86b1efca11f35a24bdbbc
nabin12468 <nabindamase12345@gmail.com>
2026-01-20 20:51:25 +0545

    setInterval

--- script3.js
+++ script3.js
@@ -1,6 +1,14 @@
 // timers and intervals
 //setTimeout,clearTimeout
-
+/*
 setTimeout(function(){
+    console.log("hello");
+}, 5000);
+
+
+*/
+
+
+setInterval(function(){
     console.log("hello");
 }, 5000);
