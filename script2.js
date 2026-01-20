2ecb14353412b2bc681f187b50e6f5e0cb90ceb9
nabin12468 <nabindamase12345@gmail.com>
2026-01-20 15:40:38 +0545

    isvalid

--- script2.js
+++ script2.js
@@ -45,20 +45,32 @@ let form = dodcument.querySelector("form");
 
 form.addEventListener("submit", function (dets){
     dets.preventDefault();
+
+
+    document.querySelector("#emailError").textContent ="";
+    document.querySelector("#passwordError").textContent ="";
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
 
 let emailans = emailRegex.test(email.value);
 let passwordans = passwordRegex.test(password.value);
 
+
+let isValid = true;
+
 if(!emailans){
     document.querySelector("#emailError").textContent = "Email is incorrect";
-    document.querySelector(".error").style.display = "initial"
+    document.querySelector("#emailError").style.display = "initial";
+    
 
 }
 if(!passwordans){
-    document.querySelector("#passwordError").textContent = "password is incorrect"
-    document.querySelector(".error").style.display = "initial";
+    document.querySelector("#passwordError").textContent = "password is incorrect";
+    document.querySelector("#passwordError").style.display = "initial";
+    
 }
 });
 
