e3eaca1e388dd44ad91515fc94fefc0bf190992e
nabin12468 <nabindamase12345@gmail.com>
2026-01-21 18:19:03 +0545

    setItem

--- script4.js
+++ script4.js
@@ -2,3 +2,30 @@
 
 //localStorage -> aapke browser ke andar data store karna jo ki browser band hone par bhi delete nahi hoga
 
+//sessionStorage -> ye aapka data temporarily store karta hai matlab ki tab band hua aur data gaya
+
+//cookies ->ye bhi data store karta hai and data browser ke cookies naam ki property mein save hota hai
+// ye cookie concept kam data ya light data ki liya hota hai
+
+
+//LocalStorage
+// store kaise kare     - setItem
+//data fetch kaise kare - getItem
+//remove kaise kare 
+//update kaise kare
+
+/*
+localStorage.setItem("name", "nabin");
+
+
+// value nikalda
+
+let val = localStorage.getItem("name");
+
+*/
+
+//for remove anything
+let val = localStorage.removeItem("name");
+
+
+
