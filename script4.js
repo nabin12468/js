6860ead047ed0a218a96019dedb57b04038b885b
nabin12468 <nabindamase12345@gmail.com>
2026-01-23 22:02:35 +0545

    window

--- script4.js
+++ script4.js
@@ -76,23 +76,31 @@ else{
 }
 setDarkOrLight();
 
+
 let btn = document.querySelector("button")
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener
+("change", function(){
+ setDarkOrLight()
+});
 
 btn.addEventListener("click", function(){
    if(document.body.classList.contains("dark")){
     document.body.classList.remove("dark");
     document.body.classList.add("light");
 
+    localStorage.setItem("theme", "light");
+
    }
    else{
     document.body.classList.remove("light");
     document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
 
    }
 });
 
+document.body.classList.add(localStorage.grtItem("theme"));
 
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener
-("change", function(){
- setDarkOrLight()
-});
+
+
