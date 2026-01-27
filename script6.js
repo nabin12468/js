c57742673a58d1b83e74acd4cb3980b219878a18
nabin12468 <nabindamase12345@gmail.com>
2026-01-27 13:06:30 +0545

    new cardsa

--- script6.js
+++ script6.js
@@ -1,1 +1,7 @@
 // noted app(save text to localStorage)
+
+// new cards create karne ha, data local storage mein karna hai
+//localstorage se hi cards ko show karna hai
+// buttons ko handle karna hai
+// filters ko handle karna hai
+
