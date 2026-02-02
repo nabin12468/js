ef8d1a02a19e171f92363ade6e474b439313be3c
nabin12468 <nabindamase12345@gmail.com>
2026-02-02 20:58:08 +0545

    scope

---
+++ script7.js
@@ -0,0 +1,18 @@
+//Scope,Execution Context and closures
+
+//variable Scope: global,block,functional
+
+// scope - functional scope, global scope and block scope
+
+// functional scope - function ke andar jo variable banta hai wo function ke andar hi accessible hota hai
+//global scope - jo variable hum function ke bahar banate hai wo globally accessible hota hai
+//block scope - {} curly braces mein hi use ho sakti ha
+
+
+/*
+function abcd(){
+    var a =  12;
+    console.log(a);
+}
+    */
+   
