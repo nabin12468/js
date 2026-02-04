312b51ab9fb3672bf118874317c35b07240b8e0a
nabin12468 <nabindamase12345@gmail.com>
2026-02-04 21:12:47 +0545

    closure definition

--- script7.js
+++ script7.js
@@ -22,3 +22,44 @@ function abcd(){
 
 //js sabse pahle jaise hi aapka function dekhta hai ssabse pahle js banaata hai execution context,ye ek process hai jo ki do different phases mein chalta hai, memory phase and doosre ka naam hai execution phase
 
+
+// lexical scope and dynamic scope
+
+// js - lexical scoping -> ki aap kaha par physically available ho ye poori tareeke se depend krta hai ki aap kya access kro paaoge
+
+function abcd(){
+  let a = 10;
+    function xyz(){
+        console.log(a);
+    }  
+}
+
+//dynamic scoping -> kaha se call kr r he ho uspe depend krega ki kya value milegi
+
+let a = 10;
+function abcd(){
+    console.log(a);
+}
+function xyz(){
+    let a = 20;
+    abcd();
+}
+xyz(); // 10
+
+//closure definition and how variable are preserved
+
+function abcd(){
+    let a = 10;
+    return function(){
+        console.log(a);
+    }
+}
+let fn = abcd();
+fn(); // 10
+
+
+//benifits of closure
+// -private variable
+// -global pollutions
+
+//how variable are preserved in closure
